4bd775f8ff09c2002404b6235db7732f6d8f48c8
beatriz <beatrizg.m@yahoo.com>
2023-05-16 14:43:48 -0300

    implementando a validaçao de CPF

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,15 @@
 class Admin < ApplicationRecord
+
+  validate :valid_cpf
+
+  def valid_cpf
+    unless CPF.valid?(cpf)
+      errors.add(:cpf, 'CPF inválido')
+    end
+  end
+
+  validates_uniqueness_of :cpf, message: 'CPF já em uso'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/system/admin_register_spec.rb
+++ spec/system/admin_register_spec.rb
@@ -4,18 +4,46 @@ describe 'Usuário se cadastra' do
   it 'com sucesso' do
     visit root_path
     click_on 'Cadastrar-se'
-    fill_in 'Email', with: 'joao@leilaodogalpao.com.br'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    fill_in 'Cpf', with: '11965580432'
+
+    fill_in 'E-mail', with: 'joao@leilaodogalpao.com.br'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    fill_in 'CPF', with: '11965580432'
     click_on 'Inscrever-se'
 
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
     expect(current_path).to eq root_path
   end
 
-  # it 'e ocorre erro' do
-  #   User.create!(email: 'joao@email.com.br', senha: 'password', cpf: '11965580000')
+  it 'com cpf já existente' do
+    ad = Admin.create!(email: 'maria@leilaodogalpao.com.br', password: 'password', password_confirmation: 'password', cpf: '11965580432')
 
-  # end
+    visit root_path
+    click_on 'Cadastrar-se'
+
+    fill_in 'E-mail', with: 'joao@leilaodogalpao.com.br'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    fill_in 'CPF', with: '11965580432'
+    click_on 'Inscrever-se'
+
+    expect(page).to have_content 'CPF já em uso'
+    expect(page).to have_content 'Não foi possível salvar usuário'
+    expect(current_path).to eq '/admins'
+    expect(page).not_to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
+  end
+
+  it 'e ocorre erro' do
+    visit root_path
+    click_on 'Cadastrar-se'
+
+    fill_in 'E-mail', with: 'joao@leilao.com.br'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    fill_in 'CPF', with: '11965580432'
+    click_on 'Inscrever-se'
+
+    expect(page).to have_content 'Não foi possível salvar usuário'
+    expect(current_path).to eq '/admins'
+  end
 end
