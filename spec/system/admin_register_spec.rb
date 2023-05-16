f2babf07c9b9ec6d96746a6499600efd8361228d
beatriz <beatrizg.m@yahoo.com>
2023-05-16 00:46:05 -0300

    Cadastrando admin com sucesso

--- spec/system/admin_register_spec.rb
+++ spec/system/admin_register_spec.rb
@@ -2,17 +2,16 @@ require "rails_helper"
 
 describe 'Usuário se cadastra' do
   it 'com sucesso' do
-    Admin.create!(email: 'joao@leilaodogalpao.com.br', senha: 'password', cpf: '11965580432')
-
     visit root_path
     click_on 'Cadastrar-se'
-    fill_in 'E-mail', with: 'joao@leilaodogalpao.com.br'
-    fill_in 'Senha', with: 'password'
-    fill_in 'CPF', with: '11965580432'
-    click_on 'Cadastrar'
+    fill_in 'Email', with: 'joao@leilaodogalpao.com.br'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Cpf', with: '11965580432'
+    click_on 'Inscrever-se'
 
-    expect(page).to have_content 'Cadastro realizado com sucesso'
-    expect(current_path).to eq 'root_path'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
   end
 
   # it 'e ocorre erro' do
